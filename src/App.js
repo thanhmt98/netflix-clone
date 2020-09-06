8aed9725c681e23a539991853f83eee1327484bc
thanhmt98 <brianmasgt@gmail.com>
2020-09-06 15:33:13 +0700

    thanhbr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Main from "./Home/Main";
+import Header from './Header/Header'
+
+/*API Key (v3 auth):     
+   2751c7f58d7dfad4c8c6ab90054ef0ed */
+/*Example API Request:
+   https://api.themoviedb.org/3/movie/550?api_key=2751c7f58d7dfad4c8c6ab90054ef0ed */
+/*API Read Access Token (v4 auth):      
+   eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzUxYzdmNThkN2RmYWQ0YzhjNmFiOTAwNTRlZjBlZCIsInN1YiI6IjVmNTQ1ZDQ4ZWY5ZDcyMDAzNGE1YjdkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QsxRsJBCeEJ684P8wlgvfbYtSCGe6Wy45uSVR4YIXD0 */
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
     </div>
   );
 }
